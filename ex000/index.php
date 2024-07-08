e9dec892919ecf918f3db8ba4696f498b112c486
Rafael <rafaelmoura7033@gmail.com>
2024-07-08 09:57:46 -0300

    ex000

---
+++ ex000/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>
+
+
+    <?php
+        print "Hello World! \u{1F30E}";
+    ?>
+    </h1>
+    
+</body>
+</html>
